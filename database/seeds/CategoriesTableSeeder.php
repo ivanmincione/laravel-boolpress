0a97f5fa0eaddb39ea5e9f76c5aba779a150f4c1
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-27 15:36:19 +0100

    Add column table Categories - Add seed Category

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -14,9 +14,8 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 5 ; $i++) {
 
-            for ($i=0; $i < 5; $i++) {
+        for ($i=0; $i < 5; $i++) {
 
             $newCategory = new Category();
             $newCategory->name = $faker->words(3, true);
@@ -26,18 +25,18 @@ class CategoriesTableSeeder extends Seeder
             // verifico che lo slug non esista nel database
             $categoryPresent = Category::where('slug', $slug)->first();
             $contatore = 1;
-            // entro nel ciclo while se ho trovato un post con lo stesso $slug
-            while($categoryPresent) {
-                // genero un nuovo slug aggiungendo il contatore alla fine
-                $slug = $slugBase . '-' . $contatore;
-                $contatore++;
-                $categoryPresent = Category::where('slug', $slug)->first();
-            }
+                // entro nel ciclo while se ho trovato un post con lo stesso $slug
+                while($categoryPresent) {
+                    // genero un nuovo slug aggiungendo il contatore alla fine
+                    $slug = $slugBase . '-' . $contatore;
+                    $contatore++;
+                    $categoryPresent = Category::where('slug', $slug)->first();
+                }
             // esco dal while se lo slug non esiste nel db
             // assegno lo slug al post
             $newCategory->slug = $slug;
             $newCategory->save();
-        }
-        }
+        }    
+
     }
 }
