215db6a04c37410e443bceff26f031424ac33e7a
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-27 15:49:34 +0100

    Add relation from table posts - categories

--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,8 @@ class Post extends Model
 {
     //
     protected $fillable =["title", "type", "description", "content", "slug" ];
+
+    public function category() {
+        return $this->belongsTo('App\Category'); // mappo la cartella secondaria
+    }
 }
---
+++ database/migrations/2021_01_27_143959_add_foreign_category_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignCategoryPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //popolo la tab e inserisco la FK
+            $table->unsignedBigInteger('category_id')->nullable()->after('slug');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_category_id_foreign'); // !!! nome tabella_nome collonna_foreign !!!
+            $table->dropColumn('category_id');
+        });
+    }
+}
