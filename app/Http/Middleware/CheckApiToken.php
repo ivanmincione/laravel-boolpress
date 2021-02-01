246c04c13369ab5acaf7fcd4d520f5fe56d41e11
Ivan Mincione <ivan.mincione92@outlook.it>
2021-02-01 18:14:59 +0100

    Add check token

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 
+use App\User;
+
 class CheckApiToken
 {
     /**
@@ -15,6 +17,25 @@ class CheckApiToken
      */
     public function handle($request, Closure $next) // all'interno del metodo "handle" va inserita tutta la logica riguardante il middleware
     {
+        $auth_token = $request->header('authorization');
+        // verifico se è presente un token di autorizzazione
+        if(empty($auth_token)) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api token mancante'
+            ]);
+        }
+        // estraggo il token dall'header
+        $api_token = substr($auth_token, 7);
+        // verifico la correttezza dell'api_token
+        $user = User::where('api_token', $api_token)->first();
+        if(!$user) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api token errato'
+            ]);
+        }
+
         return $next($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ use Illuminate\Support\Facades\Route;
 
 // creo una rotta API
 
-Route::post('/posts', 'Api\PostController@index');
+Route::post('/posts', 'Api\PostController@index')->middleware('api_token_check'); // add middleware for control
